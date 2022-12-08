9bb96dff5e75f59d33c38977b883fbc6d8c312db
PC <pkorobisyn123@gmail.com>
2022-12-08 20:51:50 +0500

    Решена задача 5

---
+++ task5/task5.py
@@ -0,0 +1,15 @@
+with open('poly_1.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('poly_2.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
+
